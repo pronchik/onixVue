2efe93e0d380c86f15c36ddbbafdaee7d31a7811
pronchik <73164330+pronchik@users.noreply.github.com>
2021-09-21 11:03:06 +0300

    Add files via upload

--- onix-main/js/script.js
+++ onix-main/js/script.js
@@ -1,5 +1,6 @@
+
 function openmenu(){
-    const leftSidebar = document.getElementsByClassName('left-sidebar'),
+    leftSidebar = document.getElementsByClassName('left-sidebar'),
     projectName = document.getElementById('project-name'),
     search = document.getElementById('search'),
     projectLogo = document.getElementById('project-logo'),
@@ -42,4 +43,84 @@ function openmenu(){
     content[0].classList.toggle('active');
 
     
+}
+
+function tasks(){
+    function myFunction(e) {
+        let elems = document.querySelectorAll(".active1");
+        [].forEach.call(elems, function(el) {
+          el.classList.remove("active1");
+        });
+      }
+    myFunction();
+    day = document.getElementById('day');
+    day.classList.toggle('active1');
+
+    message = document.getElementsByClassName('message');
+    message[0].classList.toggle('active1');
+    message[1].classList.toggle('active1');
+    message[2].classList.toggle('active1');
+
+    hr = document.getElementsByTagName('hr');
+    hr[0].classList.toggle('active1');
+    hr[1].classList.toggle('active1');
+    hr[2].classList.toggle('active1');
+
+    tip = document.getElementsByClassName('tip');
+    tip[0].classList.toggle('active1');
+
+    photo = document.getElementsByClassName('photo');
+    photo[0].classList.toggle('active1');
+    photo[1].classList.toggle('active1');
+    photo[2].classList.toggle('active1');
+    photo[3].classList.toggle('active1');
+
+}
+
+function kanban(){
+    function myFunction(e) {
+        var elems = document.querySelectorAll(".active1");
+        [].forEach.call(elems, function(el) {
+          el.classList.remove("active1");
+        });
+      }
+    myFunction();
+    kanban1 = document.getElementById('kanban');
+    kanban1.classList.toggle('active1');
+}
+
+function activity(){
+  function myFunction(e) {
+      var elems = document.querySelectorAll(".active1");
+      [].forEach.call(elems, function(el) {
+        el.classList.remove("active1");
+      });
+    }
+  myFunction();
+  activity1 = document.getElementById('activity');
+  activity1.classList.toggle('active1');
+}
+
+function files(){
+  function myFunction(e) {
+      var elems = document.querySelectorAll(".active1");
+      [].forEach.call(elems, function(el) {
+        el.classList.remove("active1");
+      });
+    }
+  myFunction();
+  files1 = document.getElementById('files');
+  files1.classList.toggle('active1');
+}
+
+function calendar(){
+  function myFunction(e) {
+      var elems = document.querySelectorAll(".active1");
+      [].forEach.call(elems, function(el) {
+        el.classList.remove("active1");
+      });
+    }
+  myFunction();
+  calendar1 = document.getElementById('calendar');
+  calendar1.classList.toggle('active1');
 }
